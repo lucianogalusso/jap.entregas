85017d92ed8253e92b19adec52c37a11db18c729
lucianogalusso <lucianogalusso@gmail.com>
2021-08-11 16:55:16 -0300

    listado productos

--- js/products.js
+++ js/products.js
@@ -4,3 +4,32 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+
+
+// const jason = "https://lucianogalusso.github.io/jason/json.json";
+
+function cargarDatosProductos(url) {
+    document.getElementById("data").innerHTML = "";
+    fetch(url)
+        .then(respuesta => respuesta.json())
+
+        .then(datos => {
+
+            let htmlContentToAppend = "";
+            for(let i = 0; i < datos.length; i++){
+                let elem = datos[i];
+
+                htmlContentToAppend += `
+                <li>`+ elem.name +` `+ elem.cost +`</li>Descripcion:<br>
+                <p>`+ elem.description +`</p>
+                `
+                 
+
+                document.getElementById("data").innerHTML = htmlContentToAppend;
+            }
+
+            
+
+        })
+        .catch(error => alert("Hubo un error: " + error));
+}
