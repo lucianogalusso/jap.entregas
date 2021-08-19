afbd52c7fe1da9641d5b910e719273dff358eb34
lucianogalusso <lucianogalusso@gmail.com>
2021-08-19 19:39:39 -0300

    login2.2

--- js/login.js
+++ js/login.js
@@ -45,17 +45,16 @@ document.addEventListener("DOMContentLoaded", function(e){
 
 	})
 
-
-	function onSignIn(googleUser) {
-
-	  var profile = googleUser.getBasicProfile();
-	  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-	  console.log('Name: ' + profile.getName());
-	  console.log('Image URL: ' + profile.getImageUrl());
-	  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-
-	  window.location = 'inicio.html';
-
-	}
-
 });
+
+function onSignIn(googleUser) {
+
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+  window.location = 'inicio.html';
+
+}
