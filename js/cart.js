97b548a53958225353b9eb28e6e30bda704eec11
lucianogalusso <lucianogalusso@gmail.com>
2021-10-06 10:13:54 -0300

    cart1.0

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,124 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+
+//variables globales de total
+let arrPrecios = [];
+
+function showCartProducts(array){
+
+    document.getElementById("cartProductos").innerHTML = "";
+
+    fetch(CART_INFO_URL) 
+        .then(respuesta => respuesta.json())
+
+        .then(datos => {
+
+            let htmlContentToAppend = "";
+            for(let i = 0; i < array.articles.length; i++){
+                let elem = array.articles[i];
+                let subtotal = elem.count*elem.unitCost;
+                let dolares = false;
+                if (elem.currency === "USD") {dolares = true}
+                arrPrecios[i] = [subtotal, dolares];	//ACA ERROR
+                alert(arrPrecios)
+
+                
+
+            	htmlContentToAppend += `
+            		<div class="list-group-item list-group-item-action">
+			            <div class="row">
+			                <div class="col-3">
+			                    <img src="` + elem.src + `" alt="` +  `" class="img-thumbnail">
+			                </div>
+			                <div class="col">
+			                    <div class="d-flex w-100 justify-content-between">
+			                        <div>
+			                            <h4 class="mb-1">`+ elem.name +`</h4>
+			                        </div>
+			                        <br>
+			                        <small class="text-muted">Cantidad: ` + elem.count + `</small><br>		                        
+			                    </div>
+			                    <h3 class="m-3">` + elem.unitCost + ` $`+ elem.currency +` </small><br>
+			                    <small class="text-muted">Subtotal:  ` + elem.count + `*`+ elem.unitCost +` $`+ elem.currency +` = 
+			                    `+ elem.count*elem.unitCost +` $`+ elem.currency +` </small>
+			                </div>
+			            </div>
+			        </div>
+		        `          
+                 
+                document.getElementById("cartProductos").innerHTML = htmlContentToAppend;
+            }
+
+
+            if (document.getElementById("cartProductos").innerHTML == "") {
+
+            	document.getElementById("cartProductos").innerHTML = `
+
+            		<div>
+            			<h4>Tu carrito se encuentra vacio actualmente</h4>
+            		</div>
+
+            	`
+
+            }else{
+
+            	showPanel();
+
+            }          
+
+        })
+        .catch(error => alert("Hubo un error: " + error));
+}
+
+function showPanel() {
+
+	document.getElementById("panelAbajo").innerHTML = "";
+	let panel = "";
+	let total = 0;
+	let subtotal = 0;
+	let envio = 0;
+
+	for (var i = 0; i < arrPrecios.length; i++) {	//con while
+		let numero = arrPrecios[i][0];
+		//let dolares = arrPrecios[i+1];
+
+		if (arrPrecios[i][1] === true) {
+
+			alert(numero*40)
+			subtotal += numero*40;
+
+		}else{
+
+			alert(numero)
+			subtotal += numero;
+
+		}
+
+
+	}
+
+	panel = `
+
+		<div>
+			<h4>Resumen de total:</h4><br><br>
+			<p>Subtotal: `+subtotal+` $UYU</p><br>
+			<p>Envio: `+subtotal/10+`</p><br><br>
+			<h4>Total: `+(subtotal + subtotal/10)+` $UYU</h4>
+		</div>
+
+	`
+
+	document.getElementById("panelAbajo").innerHTML = panel;
+
+}
+
+
 document.addEventListener("DOMContentLoaded", function(e){
+
+	getJSONData(CART_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productos = resultObj.data;
+            showCartProducts(productos);
+        }
+    });
 
 });
--- js/init.js
+++ js/init.js
@@ -6,7 +6,7 @@ const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678
 const PRODUCTOS_ACTUALIZADOS_URL = "https://lucianogalusso.github.io/jap.entregas/js/infoProducts.json";
 const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
 const PRODUCTOS_COMENTARIOS_ACTUALIZADOS_URL = "https://lucianogalusso.github.io/jap.entregas/js/comentariosInfo.json";
-const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
 var showSpinner = function(){
